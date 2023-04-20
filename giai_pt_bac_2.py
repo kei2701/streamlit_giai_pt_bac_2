c678d6cbc83a10f74831108dca811c7f22d0b4b3
Kei <89084734+kei2701@users.noreply.github.com>
2023-04-20 19:47:14 +0700

    Add files via upload

--- giai_pt_bac_2.py
+++ giai_pt_bac_2.py
@@ -1,26 +1,5 @@
 import streamlit as st
 import math
-import base64
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
 
 def gptb2(a, b, c):
     if a == 0:
@@ -48,7 +27,16 @@ def clear_input():
     st.session_state["nhap_c"] = 0.0
 
 st.subheader('Giải phương trình bậc 2')
-set_png_as_page_bg('gorou.png')
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://scontent.fsgn10-1.fna.fbcdn.net/v/t39.30808-6/280867409_1169621930516814_1805404599567449876_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=CQxIpyibgLcAX_dVydB&_nc_ht=scontent.fsgn10-1.fna&oh=00_AfCDLzJozlcLBYToLeVr7DosRtpnnkv1G_yniSyoax7BMQ&oe=64468187");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 with st.form(key='columns_in_form', clear_on_submit = False):
     a = st.number_input('Nhập a', key = 'nhap_a')
     b = st.number_input('Nhập b', key = 'nhap_b')
