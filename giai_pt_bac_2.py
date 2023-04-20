bf3ee55c2d877d2a7a5c03fe9aff3876c6761124
Kei <89084734+kei2701@users.noreply.github.com>
2023-04-20 21:27:40 +0700

    Add files via upload

--- giai_pt_bac_2.py
+++ giai_pt_bac_2.py
@@ -28,17 +28,24 @@ def clear_input():
     st.session_state["nhap_c"] = 0.0
 
 st.subheader('Giải phương trình bậc 2')
-def set_background(png_file):
-    bin_str = base64.get_base64(png_file)
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
     page_bg_img = '''
     <style>
-    .stApp {
+    body {
     background-image: url("data:image/png;base64,%s");
     background-size: cover;
     }
     </style>
     ''' % bin_str
+    
     st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
 # page_bg_img = '''
 # <style>
@@ -50,7 +57,7 @@ def set_background(png_file):
 # '''
 
 # st.markdown(page_bg_img, unsafe_allow_html=True)
-set_background('gorou.jpg')
+set_png_as_page_bg('gorou.png')
 with st.form(key='columns_in_form', clear_on_submit = False):
     a = st.number_input('Nhập a', key = 'nhap_a')
     b = st.number_input('Nhập b', key = 'nhap_b')
