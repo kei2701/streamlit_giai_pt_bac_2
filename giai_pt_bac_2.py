b368a3daafad485a995e7377cfc425937e4845ce
Kei <89084734+kei2701@users.noreply.github.com>
2023-04-20 21:32:17 +0700

    Add files via upload

--- giai_pt_bac_2.py
+++ giai_pt_bac_2.py
@@ -28,36 +28,29 @@ def clear_input():
     st.session_state["nhap_c"] = 0.0
 
 st.subheader('Giải phương trình bậc 2')
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-# page_bg_img = '''
-# <style>
-# body {
-# background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-# background-size: cover;
-# }
-# </style>
-# '''
-
-# st.markdown(page_bg_img, unsafe_allow_html=True)
-set_png_as_page_bg('gorou.png')
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+ 
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "jpg"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+set_bg_hack('gorou.jpg')
 with st.form(key='columns_in_form', clear_on_submit = False):
     a = st.number_input('Nhập a', key = 'nhap_a')
     b = st.number_input('Nhập b', key = 'nhap_b')
